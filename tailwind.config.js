58ab17e822f31ca4161d9c88206977448e25c7e4
NattawutBo <103107583+NattawutBo@users.noreply.github.com>
2025-01-14 14:37:27 +0700

    Update Tailwin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        primary: "#007bff",
-        secondary: '#6c757d',
-        success: '#28a745',
-        danger: '#dc3545',
-        warning: '#ffc107',
-        info: '#17a2b8',
-        light: '#f8f9fa',
-        dark: '#343a40',
-        tvi: '#00a350',
-      },
       fontFamily: {
-        orbitron: ['Orbitron', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#fd3d57",
       },
       width:{
-        '128': '32rem'
+        '128': '32rem',
+        '228': '42rem',
       }
     },
   },
